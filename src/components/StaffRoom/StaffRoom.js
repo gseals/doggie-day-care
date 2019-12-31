69d16c51cc38a27b1d98561632c180ae7db2a11c
Gabriel Seals <sealsgj@gmail.com>
2019-12-31 14:27:54 -0600

    employee cards rendering

--- src/components/StaffRoom/StaffRoom.js
+++ src/components/StaffRoom/StaffRoom.js
@@ -7,6 +7,19 @@ import employeeShape from '../../helpers/propz/employeeShape';
 
 class StaffRoom extends React.Component {
   static propTypes = {
-    employees
+    employees: PropTypes.arrayOf(employeeShape.employeeShape),
+  }
+
+  render() {
+    const myEmployees = this.props.employees;
+    const employeeCards = myEmployees.map((employee) => <Employee key={employee.id} employee={employee} />);
+
+    return (
+      <div className="Employees container">
+        <div>{employeeCards}</div>
+      </div>
+    );
   }
 }
+
+export default StaffRoom;
